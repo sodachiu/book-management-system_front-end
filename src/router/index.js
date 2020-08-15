835ebee113140b0ce78c309ed54ef57e32b2952f
WowJiumy <orzqll@gmail.com>
2020-08-15 16:19:37 +0800

    complete login module, and can contact with back-end

---
+++ src/router/index.js
@@ -0,0 +1,86 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '@/layout'
+
+/* Router Modules */
+
+export const constantRoutes = [
+  {
+    path: '/redirect',
+    component: Layout,
+    hidden: true,
+    children: [
+      {
+        path: '/redirect/:path(.*)',
+        component: () => import('@/views/redirect/index')
+      }
+    ]
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+  {
+    path: '/404',
+    component: () => import('@/views/error-page/404'),
+    hidden: true
+  },
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/dashboard/index'),
+        name: 'Dashboard',
+        meta: { title: 'Dashboard', icon: 'dashboard', affix: true }
+      }
+    ]
+  }
+]
+
+/**
+ * asyncRoutes
+ * the routes that need to be dynamically loaded based on user roles
+ * sideBar
+ */
+export const asyncRoutes = [
+  {
+    path: '/book',
+    component: Layout,
+    redirect: '/book/create',
+    meta: { title: '测试路由', icon: 'documentation', roles: ['admin'] },
+    children: [
+      {
+        path: '/book/create',
+        name: 'Create',
+        component: () => import('@/views/book/Views'),
+        meta: { title: '图书管理', icon: 'edit', roles: ['admin'] }
+      }
+    ]
+  },
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
+})
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
---
+++ src/views/book/Views.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>111</div>
+</template>
+
+<script>
+export default {
+  name: 'Views',
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
